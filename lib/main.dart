335c8274d3a5e6f76f1b37d68e675914547cfeb7
AYUSHDUBEY24 <mrakdubey2004@gmail.com>
2026-02-16 16:06:25 +0530

    push2

--- lib/main.dart
+++ lib/main.dart
@@ -4,114 +4,95 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ProfilePage(),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+class _MyAppState extends State<MyApp> {
+  int count = 0;
+
+  void increment() {
+    setState(() {
+      count++;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      if (count > 0) {
+        count--;
+      }
+    });
+  }
+  void reset(){
+    setState(() {
+      count=0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      appBar: AppBar(
-        title: const Text("Profile"),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: const [
-            SizedBox(height: 20),
-
-            // Profile Image
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: NetworkImage(
-                "https://i.pravatar.cc/300",
-              ),
-            ),
-
-            SizedBox(height: 20),
-
-            // Name
-            Text(
-              "Ayush Dubey",
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            SizedBox(height: 5),
-
-            // Email
-            Text(
-              "ayush@email.com",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.lightBlue,
-              ),
-            ),
-
-            SizedBox(height: 30),
-
-            // Info Cards
-            ProfileTile(
-              icon: Icons.phone,
-              title: "Phone",
-              value: "+91 9876543210",
-            ),
-
-            ProfileTile(
-              icon: Icons.location_on,
-              title: "Location",
-              value: "India",
-            ),
-
-            ProfileTile(
-
-              icon: Icons.school,
-              title: "Role",
-              value: "CS Student",
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Counter App")),
+        body: Center(
+          child: CounterWidget(
+            count: count,
+            onIncrement: increment,
+            onDecrement: decrement,
+            onReset: reset,
+          ),
         ),
       ),
     );
   }
 }
 
-class ProfileTile extends StatelessWidget {
-  final IconData icon;
-  final String title;
-  final String value;
+class CounterWidget extends StatelessWidget {
+  final int count;
+  final VoidCallback onIncrement;
+  final VoidCallback onDecrement;
+  final VoidCallback onReset;
 
-  const ProfileTile({
+  const CounterWidget({
     super.key,
-    required this.icon,
-    required this.title,
-    required this.value,
+    required this.count,
+    required this.onIncrement,
+    required this.onDecrement,
+    required this.onReset,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 12),
-      child: ListTile(
-        leading: Icon(icon),
-        title: Text(title),
-        subtitle: Text(value),
-      ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ElevatedButton(
+          onPressed: onDecrement,
+          child: const Text("-"),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          "$count",
+          style: const TextStyle(fontSize: 30),
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: onIncrement,
+          child: const Text("+"),
+        ),
+        const SizedBox(height: 20,),
+        ElevatedButton(
+          onPressed: onReset,
+          child: const Text("Reset"),
+        ),
+
+
+
+      ],
     );
   }
 }
